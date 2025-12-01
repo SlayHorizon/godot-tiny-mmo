8a0a92abffa404fa0b42b30679d5b0e5c2b62725
Carlos Alessandro <61049859+higamersslk@users.noreply.github.com>
2025-12-01 15:05:22 -0300

    feat: add day/night cycle with synchronized world clock and ambient light system
    Adds dynamic color component for maps with day/night cycle synced with WorldClock.
    
    Co-authored-by: Higaslk <iicarlosgomes477@gmail.com>

---
+++ source/common/gameplay/maps/components/ambient_light/ambient_light.gd
@@ -0,0 +1,23 @@
+class_name AmbientLight
+extends CanvasModulate
+
+@export_group("Day Night Cycle")
+@export var enabled: bool = false
+@export var light_texture: GradientTexture1D
+
+var _world_clock: WorldClockClient
+
+
+func _ready() -> void:
+    if multiplayer.is_server(): return
+    _world_clock = WorldClient.current.world_clock
+
+
+func _process(_delta: float) -> void:
+    if not enabled: return
+    if multiplayer.is_server(): return
+
+    var current_time: float = _world_clock.get_current_time()
+    var gradient_pos: float = -cos(current_time * PI / 12.0) * 0.5 + 0.5
+    
+    self.color = light_texture.gradient.sample(gradient_pos)
