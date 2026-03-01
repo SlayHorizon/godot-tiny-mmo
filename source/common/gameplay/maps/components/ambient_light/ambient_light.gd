5c486c36eb1642a52de8267d5bc966b78d903201
higamersslk <61049859+higamersslk@users.noreply.github.com>
2026-02-28 22:10:20 -0300

    ambient light: disable process frame if enabled is false.

--- source/common/gameplay/maps/components/ambient_light/ambient_light.gd
+++ source/common/gameplay/maps/components/ambient_light/ambient_light.gd
@@ -3,16 +3,19 @@ extends CanvasModulate
 
 
 @export_group("Day Night Cycle")
-@export var enabled: bool = false
 @export var light_texture: GradientTexture1D
+@export var enabled: bool:
+	set(value):
+		enabled = value
+		set_process(value)
 
 
 func _enter_tree() -> void:
 	if multiplayer.is_server():
 		queue_free()
+	set_process(enabled)
 
 
-func _process(delta: float) -> void:
-	if not enabled: return
+func _process(_delta: float) -> void:
 	var gradient_pos: float = Client.world_clock.get_day_progress()
 	self.color = light_texture.gradient.sample(gradient_pos)
